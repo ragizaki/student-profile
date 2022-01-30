59432cf6ce04770c7878a7ad2458fc75ea5a2794
Zaki Machfj <zakimachfj@gmail.com>
2022-01-30 02:05:46 -0500

    add accordion to show test grades on icon click

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './StudentCard.css';
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
 
 const fullName = (first, last) => {
     return `${first} ${last}`;
@@ -11,26 +12,48 @@ const average = grades => {
 };
 
 const StudentCard = ({ student }) => {
+    const [openCard, setOpenCard] = useState(false);
     const { pic, firstName, lastName, email, company, skill, grades } = student;
+
+    const renderTestScores = grades => {
+        return (
+            <div className='user-grades'>
+                {grades.map((grade, num) => {
+                    const toInt = Number(grade);
+                    return (
+                        <p className='user-grade'>
+                            Test {++num}: {toInt}%
+                        </p>
+                    );
+                })}
+            </div>
+        );
+    };
+
+    const handleIconClick = () => {
+        setOpenCard(prevOpenCard => !prevOpenCard);
+    };
 
     return (
         <div className='card'>
-            <main className='user'>
-                <div className='user-avatar'>
-                    <img className='user-picture' src={pic} alt='student avatar' />
-                </div>
-                <div className='user-description'>
-                    <strong>
-                        <h2 className='user-name'>{fullName(firstName, lastName)}</h2>
-                    </strong>
-                    <section className='user-caption'>
-                        <p>Email: {email}</p>
-                        <p>Company: {company}</p>
-                        <p>Skill: {skill}</p>
-                        <p>Average: {average(grades)}%</p>
-                    </section>
-                </div>
-            </main>
+            <div className='user-avatar'>
+                <img className='user-picture' src={pic} alt='student avatar' />
+            </div>
+            <header className='user-header'>
+                <h2 className='user-name'>{fullName(firstName, lastName)}</h2>
+                {openCard ? (
+                    <AiOutlineMinus onClick={handleIconClick} className='accordion-icon' />
+                ) : (
+                    <AiOutlinePlus onClick={handleIconClick} className='accordion-icon' />
+                )}
+            </header>
+            <section className='user-description'>
+                <p className='user-point'>Email: {email}</p>
+                <p className='user-point'>Company: {company}</p>
+                <p className='user-point'>Skill: {skill}</p>
+                <p className='user-point'>Average: {average(grades)}%</p>
+                {openCard && renderTestScores(grades)}
+            </section>
         </div>
     );
 };
